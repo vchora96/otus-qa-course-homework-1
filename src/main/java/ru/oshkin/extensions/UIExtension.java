1389509b27ff40744e60d1ff03830bdc10ed8a38
andrey.oshkin <vchora96@yandex.ru>
2023-02-27 21:02:07 +0300

    уменьшили версию до java до 11, прошли проверки анализаторами

--- src/main/java/ru/oshkin/extensions/UIExtension.java
+++ src/main/java/ru/oshkin/extensions/UIExtension.java
@@ -16,6 +16,8 @@ import ru.oshkin.listeners.MouseListener;
 import java.io.ByteArrayInputStream;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -56,17 +58,17 @@ public class UIExtension implements BeforeEachCallback, AfterEachCallback, After
 
                 for (Field field : fields) {
                         if (field.getType().getName().equals(WebDriver.class.getName())) {
-                                // AccessController.doPrivileged((PrivilegedAction<Void>)
-                                // () -> {
-                                try {
-                                        field.setAccessible(true);
-                                        field.set(extensionContext.getTestInstance().get(), eventRecorder);
-                                } catch (IllegalAccessException e) {
-                                        throw new Error(String.format("Could not access or set webdriver in field: %s - is this field public?", field), e);
-                                }
-                                // return null;
-                                //}
-                                //);
+                                AccessController.doPrivileged((PrivilegedAction<Void>)
+                                        () -> {
+                                                try {
+                                                        field.setAccessible(true);
+                                                        field.set(extensionContext.getTestInstance().get(), eventRecorder);
+                                                } catch (IllegalAccessException e) {
+                                                        throw new Error(String.format("Could not access or set webdriver in field: %s - is this field public?", field), e);
+                                                }
+                                                return null;
+                                        }
+                                );
                         }
                 }
         }
